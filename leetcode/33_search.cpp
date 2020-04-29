bc0be867dc865458d41277c22a0f8c7f74d37715
sun_ds <1406161541@qq.com>
2020-04-29 11:57:20 +0800

    modify

--- leetcode/33_search.cpp
+++ leetcode/33_search.cpp
@@ -38,6 +38,19 @@
  * https://leetcode-cn.com/problems/search-in-rotated-sorted-array/solution/ji-jian-solution-by-lukelee/
  *
  *
+ * 3.二分法
+ *  以旋转点为界限，数组被分成左半段和右半段升序的序列。
+ *  比较nums[mid]和nums[l]的大小。判断mid落在左半段还是右半段。
+ *  nums[mid]>=nums[l]：mid在左半段
+ *      若有 nums[l]<= target < nums[mid]
+ *          此时，target在l和mid之间，有r = mid - 1；
+ *      否则，target<nums[l]或者target>nums[mid]
+ *          target都在mid的右边，有l = mid + 1;
+ *  nums[mid]<nums[l]：mid在右半段
+ *      若有 nums[mid]< target <= nums[r]
+ *          此时，target在mid和r之间，有l = mid + 1；
+ *      否则，target>nums[r]或者target<nums[mid]
+ *          target都在mid的左边，有r = mid - 1;
  *
  */
 
@@ -101,3 +114,32 @@ public:
     }
 };
 
+//https://leetcode-cn.com/problems/search-in-rotated-sorted-array/solution/ji-bai-liao-9983de-javayong-hu-by-reedfan/
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        if(nums.size()==0)return -1;
+        int n = nums.size();
+        int l = 0, r = n-1;
+        while(l<=r){
+            int mid = l + ((r - l) >> 1);
+            if(nums[mid]==target) return mid;
+            //判断mid在左半段还是右半段
+            if(nums[mid]>=nums[l]){
+                //判断target在mid的左边还是右边
+                if(nums[l]<=target&&target<nums[mid]){
+                    r = mid - 1;
+                }else{
+                    l = mid + 1;
+                }
+            }else{
+                if(nums[mid]<target&&target<=nums[r]){
+                    l = mid + 1;
+                }else{
+                    r = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+};
