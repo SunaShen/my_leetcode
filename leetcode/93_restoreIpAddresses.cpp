8e3c8d5838813a8b617a47203cb716b16b39d4ad
sun_ds <1406161541@qq.com>
2020-05-13 14:35:43 +0800

    add some notes in 93_restoreIpAddresses.cpp

--- leetcode/93_restoreIpAddresses.cpp
+++ leetcode/93_restoreIpAddresses.cpp
@@ -1,10 +1,10 @@
 // @File   : 93_restoreIpAddresses.cpp
 // @Source : https://leetcode-cn.com/problems/restore-ip-addresses/
-// @Title  : 复原IP地址
+// @Title  : 93. 复原IP地址
 // @Auther : sun_ds
 // @Date   : 2019/11/6
 
-/*  题目描述：
+/**  题目描述：
     给定一个只包含数字的字符串，复原它并返回所有可能的 IP 地址格式。
 
     示例:
@@ -17,7 +17,7 @@
     著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 */
 
-/*
+/**
  * 复原IP地址
  *
  * 回溯法
@@ -40,58 +40,36 @@
  */
 
 
-#include <iostream>
-#include <vector>
-#include <array>
-#include <string>
-
-
-using namespace std;
-
 class Solution {
 public:
     vector<string> ans;
-    string as;
-    array<string,4> a;
+    string s;
+    array<string,4> a;//临时存储各分段
     //b当前段起始位置 t为当前为第几段
     void func(int b,int t){
         if(t==4) {
-            if(b==as.size())
+            if(b==s.size())
                 ans.push_back(a[0] + '.' + a[1] + '.' + a[2] + '.' + a[3]);
             else
                 return ;
         }
         int temp = 0;
-        if(as[b]=='0'){
+        if(s[b]=='0'){ //单独的0为一个分段
             a[t] = to_string(0);
             func(b+1,t+1);
-        }else{
-            for(int i=b;i<b+3&&i<as.size();++i){
-                temp = temp * 10 + as[i] - '0';
-                if(temp>255) continue;
+        }else{ //遍历取1,2,3位数，且都有小于等于255
+            for(int i=b;i<b+3&&i<s.size();++i){
+                temp = temp * 10 + s[i] - '0';
+                if(temp>255) break;
                 a[t] = to_string(temp);
+                //起始位置后移，段数+1
                 func(i+1,t+1);
             }
         }
-
-        return ;
     }
     vector<string> restoreIpAddresses(string s) {
-        as = s;
-
+        this->s = s;
         func(0,0);
-
         return ans;
     }
 };
-
-int main(int argc, char * argv[])
-{
-    string s = "010010";
-    Solution su;
-    auto res = su.restoreIpAddresses(s);
-    for(auto k : res)
-        cout << k << endl;
-
-    return 0;
-}
