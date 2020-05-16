88d30a3b1a9f5757f5fffcce9d5af96e7356fa6f
sun_ds <1406161541@qq.com>
2020-05-16 22:29:49 +0800

    modify some notes in 5_longestPalindrome.cpp

--- leetcode/5_longestPalindrome.cpp
+++ leetcode/5_longestPalindrome.cpp
@@ -1,10 +1,10 @@
 // @File   : 5_longestPalindrome.cpp
 // @Source : https://leetcode-cn.com/problems/longest-palindromic-substring/
-// @Title  : 最长回文子串
+// @Title  : 5. 最长回文子串
 // @Auther : sun_ds
 // @Date   : 2019/10/21
 
-/*  题目描述：
+/**  题目描述：
     给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
 
     示例 1：
@@ -22,18 +22,16 @@
     著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 */
 
-/*
+/**
  * 最长回文子串
  * 1.中心扩散，遍历每个字符，将该字符当做回文子串中心
  *   两种情况，子串为奇数，子串为偶数。
  *   保存最大的。输出
  *
  *  字符串中从pos起的n个字符，包括pos
- *  string.substr(pos,n)
- *  字符串中后i个
- *  string.substring(i)
- *  字符串中[start,end)
- *  string.substring(start,end)
+ *  str.substr(pos,n)
+ *  字符串中从pos起到末尾
+ *  str.substr(pos)
  *
  * 2.dp
  *  dp[i][j]代表，s的子串s[i..j]为回文 等于0或1
@@ -56,6 +54,7 @@ public:
         //left和right可以省略，用i(子串中间位置)和len可推出
         int left = 0, right = 0, max_len = INT_MIN;
 
+        //Right 和 Left 记录最长子串的起始位置和结束位置
         int Right = 0,Left = 0;
         //奇数
         for(int i=0;i<n;i++){
@@ -65,9 +64,9 @@ public:
                 --left;
                 ++right;
             }
+            //此时的s[left]==s[right]是不满足的。所以回文子串其实是 [left + 1 , right - 1];
             int len = right - left - 1;
             if(len > max_len){
-                cout << i << endl;
                 max_len = len;
                 Right = right - 1;
                 Left = left + 1;
@@ -89,7 +88,6 @@ public:
             }
         }
         return s.substr(Left,Right - Left + 1);
-//        return s.substring(Left,Right + 1);
     }
 };
 
@@ -104,8 +102,10 @@ public:
         int start = 0;
         vector <vector<int>> dp(len, vector<int>(len));
         for (int i = 0; i < len; i++) {
+            //初始化奇数
             dp[i][i] = 1;
             if (i < len - 1) {
+                //初始化偶数
                 if (s[i] == s[i + 1]) {
                     dp[i][i + 1] = 1;
                     start = i;
