d2f57b659496e08d3fa55b308331cddb920afe9a
sun_ds <1406161541@qq.com>
2020-09-02 22:57:25 +0800

    modify

--- 剑指offer/20_isNumber.cpp
+++ 剑指offer/20_isNumber.cpp
@@ -36,11 +36,11 @@ public:
         int e_flag = 0, pn_flag = 0, point_flag = 0, num_flag = 0;
         int i=0;
         //前面过滤空格
-        while(i<s.size()&&isspace(s[i])) i++;
+        while(i<s.size()&&s[i]==' ') i++;
         for(;i<s.size();i++){
             if(('0'<=s[i]&&s[i]<='9')){
                 num_flag = 1; //有数字了
-            }else if(s[i]=='e'){
+            }else if(s[i]=='e' || s[i]=='E'){
                 if(e_flag==0&&num_flag!=0){
                     e_flag = 1; //第1个e
                     pn_flag = 0; //e后面正负号另算
@@ -51,7 +51,7 @@ public:
             }else if(s[i]=='+'||s[i]=='-'){
                 // '+''-'只能出现在开头(所以他必须是第一个或者他前面是空格)或e后面
                 // pn_flag控制只能出现一次
-                if(pn_flag==1||(i>=1&&s[i-1] != ' '&&s[i-1] != 'e')) return false;
+                if(pn_flag==1||(i >= 1 && s[i-1] != ' ' && (s[i-1] != 'e' && s[i-1] != 'E'))) return false;
                 pn_flag = 1;
             }else if(s[i]=='.'){
                 if(point_flag==1) return false; //已经有‘.’了
@@ -76,4 +76,119 @@ public:
     }
 };
 
+// 状态机
+class Solution {
+public:
+    enum State {
+        STATE_INITIAL,
+        STATE_INT_SIGN,
+        STATE_INTEGER,
+        STATE_POINT,
+        STATE_POINT_WITHOUT_INT,
+        STATE_FRACTION,
+        STATE_EXP,
+        STATE_EXP_SIGN,
+        STATE_EXP_NUMBER,
+        STATE_END,
+    };
+
+    enum CharType {
+        CHAR_NUMBER,
+        CHAR_EXP,
+        CHAR_POINT,
+        CHAR_SIGN,
+        CHAR_SPACE,
+        CHAR_ILLEGAL,
+    };
+
+    CharType toCharType(char ch) {
+        if (ch >= '0' && ch <= '9') {
+            return CHAR_NUMBER;
+        } else if (ch == 'e' || ch == 'E') {
+            return CHAR_EXP;
+        } else if (ch == '.') {
+            return CHAR_POINT;
+        } else if (ch == '+' || ch == '-') {
+            return CHAR_SIGN;
+        } else if (ch == ' ') {
+            return CHAR_SPACE;
+        } else {
+            return CHAR_ILLEGAL;
+        }
+    }
+
+    bool isNumber(string s) {
+        unordered_map<State, unordered_map<CharType, State>> transfer{
+                {
+                        STATE_INITIAL, {
+                                               {CHAR_SPACE, STATE_INITIAL},
+                                               {CHAR_NUMBER, STATE_INTEGER},
+                                               {CHAR_POINT, STATE_POINT_WITHOUT_INT},
+                                               {CHAR_SIGN, STATE_INT_SIGN},
+                                       }
+                }, {
+                        STATE_INT_SIGN, {
+                                               {CHAR_NUMBER, STATE_INTEGER},
+                                               {CHAR_POINT, STATE_POINT_WITHOUT_INT},
+                                       }
+                }, {
+                        STATE_INTEGER, {
+                                               {CHAR_NUMBER, STATE_INTEGER},
+                                               {CHAR_EXP, STATE_EXP},
+                                               {CHAR_POINT, STATE_POINT},
+                                               {CHAR_SPACE, STATE_END},
+                                       }
+                }, {
+                        STATE_POINT, {
+                                               {CHAR_NUMBER, STATE_FRACTION},
+                                               {CHAR_EXP, STATE_EXP},
+                                               {CHAR_SPACE, STATE_END},
+                                       }
+                }, {
+                        STATE_POINT_WITHOUT_INT, {
+                                               {CHAR_NUMBER, STATE_FRACTION},
+                                       }
+                }, {
+                        STATE_FRACTION,
+                        {
+                                               {CHAR_NUMBER, STATE_FRACTION},
+                                               {CHAR_EXP, STATE_EXP},
+                                               {CHAR_SPACE, STATE_END},
+                                       }
+                }, {
+                        STATE_EXP,
+                        {
+                                               {CHAR_NUMBER, STATE_EXP_NUMBER},
+                                               {CHAR_SIGN, STATE_EXP_SIGN},
+                                       }
+                }, {
+                        STATE_EXP_SIGN, {
+                                               {CHAR_NUMBER, STATE_EXP_NUMBER},
+                                       }
+                }, {
+                        STATE_EXP_NUMBER, {
+                                               {CHAR_NUMBER, STATE_EXP_NUMBER},
+                                               {CHAR_SPACE, STATE_END},
+                                       }
+                }, {
+                        STATE_END, {
+                                               {CHAR_SPACE, STATE_END},
+                                       }
+                }
+        };
+
+        int len = s.length();
+        State st = STATE_INITIAL;
+
+        for (int i = 0; i < len; i++) {
+            CharType typ = toCharType(s[i]);
+            if (transfer[st].find(typ) == transfer[st].end()) {
+                return false;
+            } else {
+                st = transfer[st][typ];
+            }
+        }
+        return st == STATE_INTEGER || st == STATE_POINT || st == STATE_FRACTION || st == STATE_EXP_NUMBER || st == STATE_END;
+    }
+};
 
